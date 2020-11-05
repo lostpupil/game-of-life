12c925675521e93d9276e877fa6da54aa3a4a7d5
郑立_布拿拿 <zhengli@coding.net>
2020-11-05 14:00:53 +0800

    Add: use sass

--- src/index.js
+++ src/index.js
@@ -1,24 +1,93 @@
-import m from 'mithril'
+import m from 'mithril';
+import chunk from 'lodash/chunk';
 
-const root = document.getElementById('app')
+import './style.scss';
 
-const ST = {
-    world: [[0,0,0],[0,0,0],[0,0,0]]
+const root = document.getElementById('app');
+const N = 10;
+
+let MAP = []
+for (let i = 0; i < N * N; i++) {
+    MAP[i] = Math.round(Math.random())
 }
+const ST = {
+    world: chunk(MAP, N)
+}
+
 const Cell = {
-    view: () => {
-        return m('div.cell')
+    oninit: (vn) => {
+        // Cell.neighbors(vn)
+    },
+    neighbors: (vn) => {
+        const { world, x, y } = vn.attrs;
+        const allPosibleIndexes = [
+            [x - 1, y],
+            [x, y - 1],
+            [x - 1, y - 1],
+            [x + 1, y],
+            [x, y + 1],
+            [x + 1, y + 1],
+            [x + 1, y - 1],
+            [x - 1, y + 1]
+        ];
+        let allPosibleValues = [];
+
+        allPosibleIndexes.forEach(([i, j]) => {
+            try {
+                allPosibleValues.push(world[i][j])
+            } catch (err) {
+            }
+        })
+        // window.console.log(allPosibleValues)
+        return allPosibleValues.filter(v => v != undefined);
+    },
+    view: (vn) => {
+        const { status } = vn.attrs;
+        return m('.cell-wrapper', {
+            onclick: () => {
+                const {x, y, status} = vn.attrs
+                ST.world[x][y] = !status
+            }
+        },!!status ? m('div.cell.alive') : m('div.cell.dead'))
     }
 }
 
+// https://zh.wikipedia.org/wiki/%E5%BA%B7%E5%A8%81%E7%94%9F%E5%91%BD%E6%B8%B8%E6%88%8F
+// 当前细胞为存活状态时，当周围的存活细胞低于2个时（不包含2个），该细胞变成死亡状态。（模拟生命数量稀少）
+// 当前细胞为存活状态时，当周围有2个或3个存活细胞时，该细胞保持原样。
+// 当前细胞为存活状态时，当周围有超过3个存活细胞时，该细胞变成死亡状态。（模拟生命数量过多）
+// 当前细胞为死亡状态时，当周围有3个存活细胞时，该细胞变成存活状态。（模拟繁殖）
+
+
 const World = {
+    step: () => {
+
+    },
     view: () => {
-        return m('.world', ST.world.map ( (row) => {
-            return m('.row', row.map ( (col) => {
-                return m(Cell)
+        return m('.world', ST.world.map ( (row, x) => {
+            return m('.row', row.map ( (col, y) => {
+                return m(Cell, {status: col, world: ST.world, x, y})
             }))
         }))
     }
 }
 
-m.mount(root, World)
+const Observer = {
+    view: () => {
+        return m('div', [
+
+        ])
+    }
+}
+
+const Universe = {
+    view: () => {
+        return m('.wrapper', [
+            m('h1', "Game Of Life"),
+            m(World),
+            m(Observer)
+        ])
+    }
+}
+
+m.mount(root, Universe)
--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,17 @@ module.exports = {
       test: /\.(js|jsx)$/,
       include: [path.resolve(__dirname, 'src')],
       loader: 'babel-loader'
-    }]
+    }, {
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        'style-loader',
+        // Translates CSS into CommonJS
+        'css-loader',
+        // Compiles Sass to CSS
+        'sass-loader',
+      ],
+    },],
   },
 
   optimization: {
