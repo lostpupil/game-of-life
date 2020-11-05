7b6d87c6e401fd530507e687a4edf5f1830ffb42
郑立_布拿拿 <zhengli@coding.net>
2020-11-05 17:56:40 +0800

    Fix: use production build

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   plugins: [new webpack.ProgressPlugin()],
 
   module: {
