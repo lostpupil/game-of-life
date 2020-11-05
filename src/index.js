9af7d9565ad5ea939edac4166352465394210730
郑立_布拿拿 <zhengli@coding.net>
2020-11-05 15:08:15 +0800

    Add: some basic logic

--- src/index.js
+++ src/index.js
@@ -1,44 +1,40 @@
 import m from 'mithril';
 import chunk from 'lodash/chunk';
-
 import './style.scss';
 
-const root = document.getElementById('app');
-const N = 10;
+var timer;
 
+const root = document.getElementById('app');
+const N = 50;
 let MAP = []
 for (let i = 0; i < N * N; i++) {
-    MAP[i] = Math.round(Math.random())
+    MAP[i] = 0
 }
 const ST = {
     world: chunk(MAP, N)
 }
 
 const Cell = {
-    oninit: (vn) => {
-        // Cell.neighbors(vn)
-    },
-    neighbors: (vn) => {
-        const { world, x, y } = vn.attrs;
+    neighbors: (x, y) => {
         const allPosibleIndexes = [
-            [x - 1, y],
-            [x, y - 1],
-            [x - 1, y - 1],
-            [x + 1, y],
-            [x, y + 1],
-            [x + 1, y + 1],
-            [x + 1, y - 1],
-            [x - 1, y + 1]
+            [x-1, y-1],
+            [x-1, y],
+            [x-1, y+1],
+            [x, y-1],
+            [x, y+1],
+            [x+1, y-1],
+            [x+1, y],
+            [x+1, y+1],
         ];
         let allPosibleValues = [];
 
         allPosibleIndexes.forEach(([i, j]) => {
             try {
-                allPosibleValues.push(world[i][j])
+                allPosibleValues.push(ST.world[i][j])
             } catch (err) {
+                allPosibleValues.push(0)
             }
         })
-        // window.console.log(allPosibleValues)
         return allPosibleValues.filter(v => v != undefined);
     },
     view: (vn) => {
@@ -46,7 +42,7 @@ const Cell = {
         return m('.cell-wrapper', {
             onclick: () => {
                 const {x, y, status} = vn.attrs
-                ST.world[x][y] = !status
+                ST.world[x][y] = status === 1 ? 0 : 1
             }
         },!!status ? m('div.cell.alive') : m('div.cell.dead'))
     }
@@ -58,10 +54,27 @@ const Cell = {
 // 当前细胞为存活状态时，当周围有超过3个存活细胞时，该细胞变成死亡状态。（模拟生命数量过多）
 // 当前细胞为死亡状态时，当周围有3个存活细胞时，该细胞变成存活状态。（模拟繁殖）
 
-
 const World = {
     step: () => {
-
+        const next = ST.world.map( (row, x) => {
+            return row.map( (col, y) => {
+                const res = Cell.neighbors(x, y)
+                const a_cnt = res.filter( i => i === 1 ).length
+                if (col === 0) {
+                    if (a_cnt === 3) {
+                        return 1
+                    }
+                    return 0
+                } else {
+                    if (a_cnt === 2 || a_cnt === 3) {
+                        return 1
+                    }
+                    return 0
+                }
+            })
+        })
+        
+        ST.world = next
     },
     view: () => {
         return m('.world', ST.world.map ( (row, x) => {
@@ -72,10 +85,40 @@ const World = {
     }
 }
 
+
 const Observer = {
     view: () => {
-        return m('div', [
-
+        return m('div.observer', [
+            m('button', {
+                onclick: () => {
+                    ST.world =  ST.world.map( x => x.map ( (y) => Math.round(Math.random())))
+                }
+            }, 'RANDOM'),
+            m('button', {
+                onclick: () => World.step()
+            }, 'STEP'),
+            m('button', {
+                onclick: () => {
+                    if(timer){
+                        clearInterval(timer);
+                        timer = null
+                    } else {
+                        timer = setInterval(() => {
+                            World.step()
+                            m.redraw()
+                        }, 500);
+                    }
+                }
+            }, 'AUTO'),
+            m('button', {
+                onclick: () => {
+                   ST.world =  ST.world.map( x => x.map ( (y) => 0))
+                   if (timer) {
+                       clearInterval(timer)
+                       timer = null
+                   }
+                }
+            }, 'CLEAR')
         ])
     }
 }
